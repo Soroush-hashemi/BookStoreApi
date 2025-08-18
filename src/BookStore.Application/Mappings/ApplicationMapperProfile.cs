08a67965fb75d23eed643b782162801e2e0f8963
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2025-08-18 21:02:19 +0330

    Add AutoMapper profile for application DTOs

---
+++ src/BookStore.Application/Mappings/ApplicationMapperProfile.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using BookStore.Application.DTOs.Books;
+using BookStore.Application.DTOs.Categories;
+using BookStore.Application.DTOs.Sliders;
+using BookStore.Application.DTOs.ValueObjects;
+using BookStore.Domain.Entities.Books;
+using BookStore.Domain.Entities.Category;
+using BookStore.Domain.Entities.Slider;
+using BookStore.Domain.Entities.ValueObjects;
+
+namespace BookStore.Application.Mappings;
+
+public class ApplicationMapperProfile : Profile
+{
+    public ApplicationMapperProfile()
+    {
+        CreateMap<Book, BookDto>();
+        CreateMap<Book, BookUpdateDto>();
+        CreateMap<Metadata, MetadataDto>();
+        CreateMap<Category, CategoryDto>();
+        CreateMap<Category, CategoryUpdateDto>();
+
+        CreateMap<Slider, SliderDto>();
+    }
+}
