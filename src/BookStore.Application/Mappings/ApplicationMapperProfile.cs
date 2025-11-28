26e4f3c486059ff7e96cf413c4a22e2dbf5742d3
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2025-11-28 12:09:30 +0330

    Add mapping between Comment and CommentDto
    Introduced AutoMapper mapping from Comment entity to CommentDto in ApplicationMapperProfile to support comment data transformations.

--- src/BookStore.Application/Mappings/ApplicationMapperProfile.cs
+++ src/BookStore.Application/Mappings/ApplicationMapperProfile.cs
@@ -1,10 +1,12 @@
 using AutoMapper;
 using BookStore.Application.DTOs.Books;
 using BookStore.Application.DTOs.Categories;
+using BookStore.Application.DTOs.Comments;
 using BookStore.Application.DTOs.Sliders;
 using BookStore.Application.DTOs.ValueObjects;
 using BookStore.Domain.Entities.Books;
 using BookStore.Domain.Entities.Category;
+using BookStore.Domain.Entities.Comments;
 using BookStore.Domain.Entities.Slider;
 using BookStore.Domain.Entities.ValueObjects;
 
@@ -19,7 +21,7 @@ public class ApplicationMapperProfile : Profile
         CreateMap<Metadata, MetadataDto>();
         CreateMap<Category, CategoryDto>();
         CreateMap<Category, CategoryUpdateDto>();
-
+        CreateMap<Comment, CommentDto>();
         CreateMap<Slider, SliderDto>();
     }
 }
