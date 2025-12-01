1358777dd7bd15181bb2395ffdee6634a7ec0b2e
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2025-12-01 20:06:28 +0330

    Add mapping between Cart/CartItem and corresponding DTOs
    Extend ApplicationMapperProfile with AutoMapper configurations for Cart and CartItem entities, following established mapping patterns from Book, Category, and Comment mappings

--- src/BookStore.Application/Mappings/ApplicationMapperProfile.cs
+++ src/BookStore.Application/Mappings/ApplicationMapperProfile.cs
@@ -4,11 +4,13 @@ using BookStore.Application.DTOs.Categories;
 using BookStore.Application.DTOs.Comments;
 using BookStore.Application.DTOs.Sliders;
 using BookStore.Application.DTOs.ValueObjects;
+using BookStore.Application.DTOs.Carts;
 using BookStore.Domain.Entities.Books;
 using BookStore.Domain.Entities.Category;
 using BookStore.Domain.Entities.Comments;
 using BookStore.Domain.Entities.Slider;
 using BookStore.Domain.Entities.ValueObjects;
+using BookStore.Domain.Entities.Carts;
 
 namespace BookStore.Application.Mappings;
 
@@ -23,5 +25,7 @@ public class ApplicationMapperProfile : Profile
         CreateMap<Category, CategoryUpdateDto>();
         CreateMap<Comment, CommentDto>();
         CreateMap<Slider, SliderDto>();
+        CreateMap<CartItem, CartItemDto>();
+        CreateMap<Cart, CartDto>();
     }
 }
