de5d1a6abb4a680feaee555a09401c8f16fb2f50
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2025-12-02 22:54:41 +0330

    Add Id property and update CartItemDto constructor

--- src/BookStore.Application/DTOs/Carts/CartItemDto.cs
+++ src/BookStore.Application/DTOs/Carts/CartItemDto.cs
@@ -4,14 +4,17 @@ namespace BookStore.Application.DTOs.Carts;
 
 public class CartItemDto
 {
-    public CartItemDto(Guid bookId, int quantity, int unitPrice)
+    public CartItemDto(Guid id, Guid bookId,
+        int quantity, int unitPrice)
     {
+        Id = id;
         BookId = bookId;
         Quantity = quantity;
         UnitPrice = unitPrice;
         TotalPrice = quantity * unitPrice;
     }
 
+    public Guid Id { get; private set; }
     public Guid BookId { get; private set; }
     public int Quantity { get; private set; }
     public int UnitPrice { get; private set; }
