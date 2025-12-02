e06a9056135b762af0e03276f114fce1f773fa91
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2025-12-02 22:53:45 +0330

    Simplify CartDto constructor and change Items to List

--- src/BookStore.Application/DTOs/Carts/CartDto.cs
+++ src/BookStore.Application/DTOs/Carts/CartDto.cs
@@ -6,16 +6,16 @@ namespace BookStore.Application.DTOs.Carts;
 
 public class CartDto
 {
-    public CartDto(Guid userId, IEnumerable<CartItemDto> items)
+    public CartDto(Guid userId)
     {
         UserId = userId;
-        Items = (items ?? Enumerable.Empty<CartItemDto>()).ToList().AsReadOnly();
+        Items = new List<CartItemDto>();
         TotalItems = Items.Sum(i => i.Quantity);
         TotalPrice = Items.Sum(i => i.TotalPrice);
     }
 
     public Guid UserId { get; private set; }
-    public IReadOnlyCollection<CartItemDto> Items { get; private set; }
+    public List<CartItemDto> Items { get; private set; }
     public int TotalItems { get; private set; }
     public int TotalPrice { get; private set; }
 }
