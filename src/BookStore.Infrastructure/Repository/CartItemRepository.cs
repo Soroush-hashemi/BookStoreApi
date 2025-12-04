d21417bdb4170e00fb0e0009e96b33856d50ca87
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2025-12-04 16:09:00 +0330

    Remove custom methods from CartItemRepository implementation

--- src/BookStore.Infrastructure/Repository/CartItemRepository.cs
+++ src/BookStore.Infrastructure/Repository/CartItemRepository.cs
@@ -13,27 +13,4 @@ public class CartItemRepository : BaseRepository<CartItem>, ICartItemRepository
     {
         _dbContext = dbContext;
     }
-
-    public async Task<CartItem> GetItemById(Guid CartItemId)
-    {
-        var cartItem = await _dbContext.CartItem
-            .FirstOrDefaultAsync(c => c.Id == CartItemId);
-        if (cartItem is null)
-            throw new NullReferenceException("cartItem doesn't Exist");
-
-        return cartItem;
-    }
-
-    public async Task<CartItem> ItemIsExistInCart(Guid CartItemId, Guid BookId)
-    {
-        var cartItem = await _dbContext.CartItem
-            .FirstOrDefaultAsync(c => c.Id == CartItemId);
-
-        cartItem
-
-        if (cartItem is null)
-            throw new NullReferenceException("cartItem doesn't Exist");
-
-        return cartItem;
-    }
 }
