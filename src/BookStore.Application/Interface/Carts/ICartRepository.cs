d4344e7cded64311a8c23189299104cb1115f5e6
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2025-12-04 15:53:24 +0330

    Simplify ICartRepository interface by removing item management methods

--- src/BookStore.Application/Interface/Carts/ICartRepository.cs
+++ src/BookStore.Application/Interface/Carts/ICartRepository.cs
@@ -6,11 +6,4 @@ namespace BookStore.Application.Interface;
 public interface ICartRepository : IBaseRepository<Cart>
 {
     Task<Cart> GetByUserIdAsync(Guid userId);
-    Task<bool> UserHasCartAsync(Guid userId);
-    Task<Cart> AddOrUpdateItemAsync(Guid userId, CartItem item);
-
-    Task RemoveItemAsync(Guid userId, Guid bookId);
-    Task UpdateItemQuantityAsync(Guid userId, Guid bookId, int newQuantity);
-    Task UpdateItemPriceAsync(Guid userId, Guid bookId, int newUnitPrice);
-    Task ClearCartAsync(Guid userId);
 }
